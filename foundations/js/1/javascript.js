68fa85fa08f3595b7403a879a0c5b6ca3d7ee7a2
baglana <102351104+baglana@users.noreply.github.com>
2023-09-14 00:00:47 +0600

    Finish foundations-fundamentals-part-1 assignment

--- foundations/js/1/javascript.js
+++ foundations/js/1/javascript.js
@@ -1,1 +1,22 @@
-console.log('Hello World');
+// console.log('Hello World');
+
+// 1. Add 2 numbers together! (just type console.log(23 + 97) into your html file)
+
+// console.log(23 + 97);
+
+
+// 2. Add a sequence of 6 different numbers together.
+
+// console.log(-4 + 8 + 3 + 100 + 493 + -399);
+
+
+// 3. Print the solution to the following equation: (4 + 6 + 9) / 77
+// Answer should be approximately 0.24675
+
+// console.log((4 + 6 + 9) / 77);
+
+// 5.
+
+// const max = 57;
+// let actual = max - 13;
+// let percentage = actual / max;
