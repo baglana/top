4e6057eae266594fb7d80e6197036f3342c84627
baglana <102351104+baglana@users.noreply.github.com>
2023-09-28 13:21:33 +0600

    feat(foundations/js/4-dom): add red paragraph

--- foundations/js/4-dom/script.js
+++ foundations/js/4-dom/script.js
@@ -5,3 +5,10 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+// 1. a <p> with red text that says “Hey I’m red!”
+const para = document.createElement('p');
+para.style.color = 'red';
+para.textContent = 'Hey I’m red';
+
+container.appendChild(para);
